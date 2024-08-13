44d75a82fc6ab2f5d52ba482ccc6063941343d95
MaxsuelsSouza <maxsuel.souza@equipeblue.com.br>
2024-08-13 13:34:44 -0300

    commit inicial

---
+++ src/Share/components/BtnInserirComponents.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    emits: ['inserir'],
+
+    props: {
+        outlined: {
+            type: Boolean,
+            default: false
+        },
+
+        label: {
+            type: String,
+            default: () => 'Inserir'
+        },
+
+        route: {
+            type: Object,
+            default: () => null
+        },
+
+        icone: {
+            type: String,
+            default: 'pi pi-plus'
+        }
+    },
+
+    methods: {
+        inserir() {
+            if (this.route) {
+                this.$router.push(this.route);
+                return;
+            }
+            this.$emit('inserir');
+        }
+    }
+};
+</script>
+
+<template>
+    <Button :label="label" size="large" :icon="icone" :class="`text-base bg-blue-700`" @click="inserir()" />
+</template>
